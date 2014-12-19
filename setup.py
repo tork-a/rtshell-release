8e71f0fc6340b11c8105d3168a4af887b71a96de
Kei Okada <kei.okada@gmail.com>
2014-12-19 17:38:49 +0900

    Rebase from 'release/indigo/rtshell'

--- setup.py
+++ setup.py
@@ -43,7 +43,8 @@ def get_files(dir, ext=None):
 
 if sys.platform != 'win32':
     cwd = os.path.join(os.getcwd(), 'doc')
-    s = raw_input('Generate documentation? ')
+    #s = raw_input('Generate documentation? ')
+    s = 'y'
     if s.lower() == 'y' or s.lower() == 'YES':
         print 'Generating documentation'
         p = subprocess.Popen(['./make_docs', 'man', 'html', 'pdf', '-v'],
